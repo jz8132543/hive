5f55bb0f72baa4a8e88ad7e1cc5b1b025242a336
jz8132543 <i@dora.im>
2023-02-02 00:43:22 +0800

    test

--- nix/tippy/nixosConfigurations/surface/default.nix
+++ nix/tippy/nixosConfigurations/surface/default.nix
@@ -1,6 +1,13 @@
 { inputs, cell, ... }:
 
 rec {
+  bee.system = "x86_64-linux";
+  bee.home = inputs.home;
+  bee.pkgs = import inputs.nixos {
+    inherit (inputs.nixpkgs) system;
+    config.allowUnfree = true;
+    overlays = cell.overlays.desktop;
+  };
   imports = [
     ./hardware-configuration.nix
   ]
